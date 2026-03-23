e93d7aa95b5bbae59992e19338ff8a02906f2336
Benno Flory <benno.flory@gmx.ch>
2026-03-23 15:45:21 +0100

    v1.0.2, 9.2022: add widget configuration (not working yet, config of twingle manager is overriding it)

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,21 +17,37 @@ $document   = JFactory::getDocument();
 $project_id = $params->get('project_id', '0');
 $type       = $params->get('type', 'widget');
 $id         = '_' . ModTwingleHelper::generateRandomString(9);
+$base_url   = 'https://spenden.twingle.de/embed/genericorganisation/genericproject';
+
+// Get presets and pass to script loading url
+$tw_params = [];
+$tw_params['tw_amount'] = $params->get('tw_amount', '');
+$tw_params['tw_target'] = $params->get('tw_target', '');
+$tw_params['tw_rhythm'] = $params->get('tw_rhythm', '');
+$tw_params['tw_min']    = $params->get('tw_min', '');
+$tw_params['tw_max']    = $params->get('tw_max', '');
+$url_params = [];
+foreach (array_filter($tw_params) as $tw_key => $tw_value) {
+  if ($tw_value != 'undefined') {
+    $url_params[] = $tw_key . '=' . $tw_value;
+  }
+}
+$url_params = implode('&', $url_params);
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
 
 /* Load external JS only once per page */
-if (!$twingle_js_loaded) {
-//  $document->addScript('https://spenden.twingle.de/embed/generic');
-  $document->addScriptDeclaration('
+//if (!$twingle_js_loaded) {
+    $document->addScriptDeclaration('
     (function() {
-      var u="https://spenden.twingle.de/embed/genericorganisation/genericproject/' . $project_id . '/' . $type . '/' . $id . '";
+//      var u="' . $base_url . '/' . $project_id . '/' . $type . '/' . $id . '?' . $url_params . '"; // Buggy, form doesn´t load...
+      var u="' . $base_url . '/' . $project_id . '/' . $type . '/' . $id . '";
       var d=document, g=d.createElement(\'script\'), s=d.getElementsByTagName(\'script\')[0];
       g.type=\'text/javascript\'; g.async=true; g.defer=true; g.src=u; s.parentNode.insertBefore(g,s);
     })();
   ');
-  $twingle_js_loaded = true;
-}
+//  $twingle_js_loaded = true;
+//}
 ?>
 
 <!-- twingle -->
